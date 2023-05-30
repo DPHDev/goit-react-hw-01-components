61c4069d2e60983fa19fe3bc4556923d61284d92
DanielPerdomoDev <dfperdomodev@gmail.com>
2023-05-30 03:58:42 -0500

    task 1 finish

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import user from './tarea1/utils/user.json';
+import { UserProfile } from './tarea1/components/UserProfile';
 import './index.css';
+
+// --------------------------------------------------------------
+// -------- Lógica para procesar los datos del Json -------------
+// --------------------------------------------------------------
+const userData = {
+  name: user.username,
+  tag: user.tag,
+  location: user.location,
+  avatar: user.avatar,
+  followers: user.stats.followers,
+  views: user.stats.views,
+  likes: user.stats.likes,
+};
+// --------------------------------------------------------------
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <UserProfile avatarUrl={userData.avatar} userName={userData.username} userTag={userData.tag} userLocation={userData.location} userFollowers={userData.followers} userViews={userData.views} userLikes={userData.likes}/>
   </React.StrictMode>
 );
---
+++ src/tarea1/components/UserProfile.jsx
@@ -0,0 +1,29 @@
+import './user-profile.css';
+
+export const UserProfile = ({avatarUrl, userName, userTag, userLocation, userFollowers, userViews, userLikes}) => {
+  return (
+    <div class="profile">
+      <div class="description">
+        <img src={avatarUrl} alt="User avatar" class="avatar" />
+        <p class="name">{userName}</p>
+        <p class="tag">@{userTag}</p>
+        <p class="location">{userLocation}</p>
+      </div>
+
+      <ul class="stats">
+        <li>
+          <span class="label">Followers</span>
+          <span class="quantity">{userFollowers}</span>
+        </li>
+        <li>
+          <span class="label">Views</span>
+          <span class="quantity ">{userViews}</span>
+        </li>
+        <li>
+          <span class="label">Likes</span>
+          <span class="quantity">{userLikes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
